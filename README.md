5af01bf92de8a020cd7ce3a1b5e6583b74dd50a7
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-01-13 12:17:59 +0530

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # unsplash-download
 Simple script to download images from unsplash.com
+
+## Dependencies
+
+This program uses [Python3](), [Geckodriver]() and [wget]().
+Add your `geckodriver` in $PATH.
+
+### Install required modules
+
+It is recommended to create a virtual environment first.
+
+```
+$ pip3 install -r requirements.txt
+```
+
+## Running
+
+```
+$ python3 unsplash.py
+```
---
+++ unsplash.py
@@ -0,0 +1,24 @@
+# script to download photos from unsplash
+
+from bs4 import BeautifulSoup
+from splinter import Browser
+import os
+
+tag = input("Enter tag : ")
+link = "https://unsplash.com/search/photos/"+tag
+
+with Browser("firefox",headless=True) as browser:
+	browser.visit(link)
+	html = browser.html
+	soup = BeautifulSoup(html, "html.parser")
+	allImages = [x.get("src") for x in soup.findAll("img")]
+	images = [x.split("?")[0] for x in allImages if "images.unsplash.com" in x]
+	# wget the files
+	for x in images:
+		os.system("wget --no-check-certificate -c -P photos/ {}".format(tag, x))
+
+# the files are without any extension
+
+files = os.listdir("photos")
+for f in files:
+	f.rename(f,f+"jpg")
