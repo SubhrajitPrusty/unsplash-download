7da0092af1960686cddcb72b6aa6c7e3b2d6999b
Dibya ranjan jena <dibyajena917@gmail.com>
2018-10-02 21:32:19 +0530

    removed splinter and added requests support

--- unsplash.py
+++ unsplash.py
@@ -2,7 +2,7 @@
 import sys
 
 from bs4 import BeautifulSoup
-from splinter import Browser
+import requests
 import os
 
 if len(sys.argv) > 1:
@@ -14,23 +14,21 @@ if len(sys.argv) > 1:
 	link = "https://unsplash.com/search/photos/"+tag
 
 	print("Downloading images for {}...".format(tag))
-
-	with Browser("firefox",headless=True) as browser:
-		browser.visit(link)
-		html = browser.html
-		soup = BeautifulSoup(html, "html.parser")
-		if limit:
-			allImages = [x.get("src") for x in soup.findAll("img")][:limit]
-		else
-			allImages = [x.get("src") for x in soup.findAll("img")]
-		images = [x.split("?")[0] for x in allImages if "images.unsplash.com" in x]
-		# wget the files
-		for x in images:
-			os.system("wget -q --no-check-certificate -c -P photos/{} {}".format(tag, x))
+	r=requests.get(link)
+	html=r.content
+	soup = BeautifulSoup(html, "html.parser")
+	if limit:
+		allImages = [x.get("src") for x in soup.findAll("img")][:limit]
+	else:
+		allImages = [x.get("src") for x in soup.findAll("img")]
+	images = [x.split("?")[0] for x in allImages if "images.unsplash.com" in x and "profile" not in x]
+	# wget the files
+	for x in images:
+		os.system("wget -q --no-check-certificate -c -P photos/{} {}".format(tag, x))
 
 	# the files are without any extension
 
-	os.chdir("photos/"+tag)
+	os.chdir("photos/"+tag.split()[0])
 	files = os.listdir(".")
 	for f in files:
 		if not f.endswith(".jpg"):
